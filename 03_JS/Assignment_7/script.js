e3df61fc344b2a6e60e85e44a90152281fe9635a
RANA-ABDUL-REHMAN-RAUF <shadowhighwayyt@gmail.com>
2026-02-25 16:00:35 +0500

    created switch function

--- 03_JS/Assignment_7/script.js
+++ 03_JS/Assignment_7/script.js
@@ -11,3 +11,34 @@ let ifElse = () => {
     }
 }
 
+let switchCase = () => {
+    let day = new Date().getDay();
+    let dayName;
+    switch (day) {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+            dayName = "Unknown";
+    }
+    output.innerHTML = "Today is " + dayName;
+}
+
