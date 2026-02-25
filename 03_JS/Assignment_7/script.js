cd3912c655cb69c6c903a1c7f2fc56b3f4de5dca
RANA-ABDUL-REHMAN-RAUF <shadowhighwayyt@gmail.com>
2026-02-25 15:59:22 +0500

    created if else function

--- 03_JS/Assignment_7/script.js
+++ 03_JS/Assignment_7/script.js
@@ -2,3 +2,12 @@ let clearOutput = () => {
     document.getElementById("Output").innerHTML = ""
 }
 let output = document.querySelector("#Output");
+let ifElse = () => {
+    let day = new Date().getDay();
+    if (day === 0 || day === 6) {
+        output.innerHTML = "It's the weekend!";
+    } else {
+        output.innerHTML = "It's a weekday.";
+    }
+}
+
