80bb0e1fc79027bc22abb613e3c1eea573eb6a32
RANA-ABDUL-REHMAN-RAUF <shadowhighwayyt@gmail.com>
2026-02-28 13:31:08 +0500

    created toast error function

--- 03_JS/Assignment_8/script.js
+++ 03_JS/Assignment_8/script.js
@@ -3,3 +3,17 @@ let clearOutput = () => {
     document.getElementById("Output").innerHTML = ""
 }
 let output = document.querySelector("#Output");
+let toastError = (alert) => {
+    Toastify({
+        text: alert,
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right,rgb(255, 0, 0),rgb(0, 0, 0))",
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
+}
